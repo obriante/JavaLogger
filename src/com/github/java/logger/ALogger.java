bc76151f28f8e12ccf8c06b720d1d80c315ec154
Orazio Briante <obriante@users.noreply.github.com>
2023-03-13 14:22:38 +0100

    Change package name

--- src/com/github/java/logger/ALogger.java
+++ src/com/github/java/logger/ALogger.java
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package com.github.kernelmonkey.java.logger;
+package com.github.java.logger;
 
 import java.io.PrintStream;
 import java.io.PrintWriter;
